8d4a6ffdd57efad0dd87e2eed58195d7a328cfa5
Justin Barclay <justincbarclay@gmail.com>
2016-10-12 21:57:38 -0600

    Add getters and setters to requests

--- app/src/main/java/ca/ualberta/ridr/Request.java
+++ app/src/main/java/ca/ualberta/ridr/Request.java
@@ -1,16 +1,27 @@
 package ca.ualberta.ridr;
 
+import java.util.ArrayList;
+
 /**
  * Created by Justin on 2016-10-12.
  */
 public class Request {
+    private String ridr;
     private String pickup;
     private String dropoff;
+    private ArrayList<Driver> possibleDrivers;
+    private Boolean accepted;
+    private Driver driver;
 
-    Request(String pickup, String Dropoff){
+    Request(String pickup, String dropoff){
         this.pickup = pickup;
         this.dropoff = dropoff;
     }
+
+    public void setDropoff(String dropoff) {
+        this.dropoff = dropoff;
+    }
+
 
     public String getPickup() {
         return pickup;
@@ -18,5 +29,44 @@ public class Request {
 
     public String getDropoff(){
         return dropoff;
+    }
+
+    public void addAccepted(Driver driver) {
+    }
+
+    public String getRidr() {
+        return ridr;
+    }
+
+    public void setRidr(String ridr) {
+        this.ridr = ridr;
+    }
+
+    public void setPickup(String pickup) {
+        this.pickup = pickup;
+    }
+
+    public ArrayList<Driver> getPossibleDrivers() {
+        return possibleDrivers;
+    }
+
+    public void setPossibleDrivers(ArrayList<Driver> drivers) {
+        this.possibleDrivers = drivers;
+    }
+
+    public Boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(Boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
     }
 }
