3ddece77009b6eec5f9892088c7a0949c2934f0a
Justin Barclay <justincbarclay@gmail.com>
2016-11-05 21:27:05 -0600

    Add toggle buttons for login

--- app/src/main/java/ca/ualberta/ridr/LoginView.java
+++ app/src/main/java/ca/ualberta/ridr/LoginView.java
@@ -1,14 +1,42 @@
 package ca.ualberta.ridr;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.R.attr.type;
 
 public class LoginView extends Activity {
+    TextView riderLogin;
+    TextView driverLogin;
+    boolean asDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        asDriver = true;
+        driverLogin = (TextView) findViewById(R.id.DriverLogin);
+        riderLogin = (TextView) findViewById(R.id.RiderLogin);
+
+        driverLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                asDriver = !asDriver;
+                if (asDriver) {
+                    driverLogin.setBackgroundResource(R.drawable.selected_login_button);
+                    driverLogin.setTextColor(ContextCompat.getColor(getBaseContext(), R.color.tertiary_colour));
+                    riderLogin.setBackgroundResource(R.drawable.login_button_border);
+                    riderLogin.setTextColor(ContextCompat.getColor(getBaseContext(), R.color.light_tertiary_colour));
+                } else {
+                    driverLogin.setBackgroundResource(R.drawable.login_button_border);
+                    driverLogin.setTextColor(ContextCompat.getColor(getBaseContext(), R.color.light_tertiary_colour));
+                    riderLogin.setBackgroundResource(R.drawable.selected_login_button);
+                    riderLogin.setTextColor(ContextCompat.getColor(getBaseContext(), R.color.tertiary_colour));
+                }
+            }
+        });
     }
 }
