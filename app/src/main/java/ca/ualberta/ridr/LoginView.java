0956858170a018dad75168d93c9be08a50c31e5d
Justin Barclay <justincbarclay@gmail.com>
2016-11-05 22:03:51 -0600

    Add colour change on submit button press

--- app/src/main/java/ca/ualberta/ridr/LoginView.java
+++ app/src/main/java/ca/ualberta/ridr/LoginView.java
@@ -3,7 +3,10 @@ package ca.ualberta.ridr;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static android.R.attr.type;
@@ -11,6 +14,7 @@ import static android.R.attr.type;
 public class LoginView extends Activity {
     TextView riderLogin;
     TextView driverLogin;
+    Button loginButton;
     boolean asDriver;
 
     @Override
@@ -21,9 +25,25 @@ public class LoginView extends Activity {
         driverLogin = (TextView) findViewById(R.id.DriverLogin);
         riderLogin = (TextView) findViewById(R.id.RiderLogin);
 
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    loginButton.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.dark_accent));
+                } else if(event.getAction() == MotionEvent.ACTION_UP) {
+                    loginButton.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.tertiary_colour));
+                }
+                return false;
+            }
+        });
         driverLogin.setOnClickListener(changeUserType);
         riderLogin.setOnClickListener(changeUserType);
+
+
     }
+
 
     View.OnClickListener changeUserType = new View.OnClickListener() {
         @Override
