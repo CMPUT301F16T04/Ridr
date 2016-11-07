81edcd2279e6bef860ecd99af39a70b98657f808
mack1070101 <mackenziebligh@gmail.com>
2016-11-07 16:20:58 -0700

    add work on view

--- app/src/main/java/ca/ualberta/ridr/RiderRequestView.java
+++ app/src/main/java/ca/ualberta/ridr/RiderRequestView.java
@@ -2,12 +2,33 @@ package ca.ualberta.ridr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class RiderRequestView extends AppCompatActivity {
-
+    private ListView oldRequestsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rider_request);
+
+        Button menuButton = (Button)(findViewById(R.id.menu_button));
+        oldRequestsList = (ListView)(findViewById(R.id.oldRequestLists));
+
+        menuButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setResult(RESULT_OK);
+                //Open a pop up menu
+
+            }
+        });
     }
+
+    protected void onStart(){
+        //populate list
+    }
+
+
 }
