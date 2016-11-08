a17bfbc62bf35a9053b52c1f028a6c1a358c5ac6
skaefer143 <stormkaefer@gmail.com>
2016-11-07 20:19:40 -0700

    started view work, going to merge with master now

---
+++ app/src/main/java/ca/ualberta/ridr/AddUser.java
@@ -0,0 +1,13 @@
+package ca.ualberta.ridr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddUser extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_user);
+    }
+}
