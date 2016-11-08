0595bc2ac4d622952d96cb67cd2f7e9231e3d0db
Kristy <knewbury@ualberta.ca>
2016-11-07 21:52:14 -0800

    added rough widgets to AcceptRiderView

--- app/src/main/java/ca/ualberta/ridr/AcceptRiderView.java
+++ app/src/main/java/ca/ualberta/ridr/AcceptRiderView.java
@@ -2,12 +2,29 @@ package ca.ualberta.ridr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AcceptRiderView extends AppCompatActivity {
+
+    private TextView requestInfo;
+    private TextView requestFrom;
+    private CharSequence isFrom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accept_rider);
+
+        requestInfo = (TextView) findViewById(R.id.possible_request_info);
+        requestFrom = (TextView) findViewById(R.id.request_from);
+
+        Request request = null;
+
+        isFrom = "Request From"+ request.getRider();
+
+        requestFrom.setText(isFrom);
+
+        requestFrom.setOnClickListener(View.OnClickListener);
     }
 }
