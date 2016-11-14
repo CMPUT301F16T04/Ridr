333131fe0c4a622ba14e60d0746fc54129dd95c1
Kristy <knewbury@ualberta.ca>
2016-11-13 23:02:11 -0800

    made the intergration work

--- app/src/main/java/ca/ualberta/ridr/AcceptDriverView.java
+++ app/src/main/java/ca/ualberta/ridr/AcceptDriverView.java
@@ -54,22 +54,22 @@ public class AcceptDriverView extends Activity {
         accept = (Button) findViewById(R.id.accept_button);
 
         //this is to fetch the data we need from the previous activity
-//        Intent intent = getIntent();
-//        ArrayList<String> ids = intent.getStringArrayListExtra("ids");
-//        if (ids != null) {
-//            riderId= ids.get(0);
-//            driverId= ids.get(1);
-//            requestId = ids.get(2);
-//        }
-//        else{
-//            //this is if there was some error retrieving data passed, just go back to prev activity
-//            finish();
-//        }
+        Intent intent = getIntent();
+        ArrayList<String> ids = intent.getStringArrayListExtra("ids");
+        if (ids != null) {
+            riderId= ids.get(0);
+            driverId= ids.get(1);
+            requestId = ids.get(2);
+        }
+        else{
+            //this is if there was some error retrieving data passed, just go back to prev activity
+            finish();
+        }
 
 //just here for testing, might leave for now.
-        final String riderId = "6a5f339c-2679-4e18-825f-2d6fc6cdc3e2";
-        final String driverId = "475a3caa-88b5-46b2-9a44-cd02ef8a2d28";
-        final String requestId = "4d08b0e5-9bf7-45fb-b5ea-37a5cb03eeba";
+//        final String riderId = "6a5f339c-2679-4e18-825f-2d6fc6cdc3e2";
+//        final String driverId = "475a3caa-88b5-46b2-9a44-cd02ef8a2d28";
+//        final String requestId = "4d08b0e5-9bf7-45fb-b5ea-37a5cb03eeba";
 
 
         final Driver driver = getDriver(driverId);
--- app/src/main/java/ca/ualberta/ridr/RiderRequestView.java
+++ app/src/main/java/ca/ualberta/ridr/RiderRequestView.java
@@ -1,17 +1,33 @@
 package ca.ualberta.ridr;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class RiderRequestView extends Activity {
+
+    private Activity activity = this;
     public ArrayList<Request> requests = new ArrayList<>();
     //Declaring reference buttons in the GUI
     ListView oldRequestsList;
@@ -40,7 +56,85 @@ public class RiderRequestView extends Activity {
         }
 
 
-        RequestAdapter customAdapter = new RequestAdapter(this, requests);
+        RequestAdapter customAdapter = new RequestAdapter(activity, requests);
         oldRequestsList.setAdapter(customAdapter);
+
+        //this is to recognize listview item presses within the view
+        oldRequestsList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position ,long id){
+                Request request = requests.get(position);
+                displayDrivers(request);
+            }
+        });
+
+
+    }
+
+    //thinking of popup window as outlined in http://stackoverflow.com/questions/15153651/set-own-layout-in-popup-window-in-android
+    //date link accessed : Nov 5 2016
+    //author: Emil Adz ,edited Vladimir Kulyk
+    public void displayDrivers(Request request){
+
+        // Inflate the popup_layout.xml
+        LinearLayout viewGroup = (LinearLayout) findViewById(R.id.drivers_who_accepted);
+        LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.drivers_who_accepted, viewGroup);
+
+        // Creating the PopupWindow
+        final PopupWindow driverPopUp = new PopupWindow(activity);
+        driverPopUp.setContentView(layout);
+        driverPopUp.setWidth(LinearLayout.LayoutParams.WRAP_CONTENT);
+        driverPopUp.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+        driverPopUp.setFocusable(true);
+
+        // Some offset to align the popup a bit to the left, and a bit down, relative to button's position.
+        int OFFSET_X = 60;
+        int OFFSET_Y = 600;
+
+
+        // Displaying the popup at the specified location, + offsets.
+        driverPopUp.showAtLocation(layout, Gravity.NO_GRAVITY,  OFFSET_X,  OFFSET_Y);
+
+
+        // Getting a reference to Close button, and close the popup when clicked.
+        Button close = (Button) layout.findViewById(R.id.exit_popup);
+        close.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                driverPopUp.dismiss();
+            }
+        });
+
+        ArrayList<Driver> possibleDrivers =  new ArrayList<>();
+        Driver driver =  new Driver("name", new Date(), "","","","");
+        possibleDrivers.add(driver);
+
+
+        ListView popupList = (ListView) layout.findViewById(R.id.drivers_list);
+        ArrayAdapter<Driver> adapter_popup = new ArrayAdapter<Driver>(activity, R.layout.driver_who_accepted, possibleDrivers);
+        popupList.setAdapter(adapter_popup);
+
+        //this is to recognize listview item presses within the popup
+        popupList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position , long id) {
+                String riderId = "8e16686b-f72d-42e1-90ea-e7a8cf270732"; //"6a5f339c-2679-4e18-825f-2d6fc6cdc3e2";
+                String driverId = "475a3caa-88b5-46b2-9a44-cd02ef8a2d28";
+                String requestId = "4d08b0e5-9bf7-45fb-b5ea-37a5cb03eeba";
+
+                //must close popup before going to next activity
+                driverPopUp.dismiss();
+
+                //TODO pass the driver at clicked position to the next activity
+                Intent intent = new Intent(activity, AcceptDriverView.class);
+                ArrayList<String> ids = new ArrayList<>();
+                ids.add(riderId);
+                ids.add(driverId);
+                ids.add(requestId);
+                intent.putStringArrayListExtra("ids", ids );
+                startActivity(intent);
+                //go to driver profile
+            }
+        });
     }
 }
