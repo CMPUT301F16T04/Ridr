0dfe4a368d04c39392e1034fbe8c7fb08f473316
Justin Barclay <justincbarclay@gmail.com>
2016-11-20 20:07:36 -0700

    Initial setup for branch

---
+++ app/src/main/java/ca/ualberta/ridr/DriverRideView.java
@@ -0,0 +1,14 @@
+package ca.ualberta.ridr;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DriverRideView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_ride_view);
+    }
+
+}
