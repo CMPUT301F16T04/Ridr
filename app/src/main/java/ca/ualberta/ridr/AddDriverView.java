5ed32183b088e60e344d613048c7f67a3bd95b5f
Justin Barclay <justincbarclay@gmail.com>
2016-11-23 01:26:08 -0700

    In the middle of a refactor

--- app/src/main/java/ca/ualberta/ridr/AddDriverView.java
+++ app/src/main/java/ca/ualberta/ridr/AddDriverView.java
@@ -1,7 +1,9 @@
 package ca.ualberta.ridr;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.telephony.PhoneNumberUtils;
@@ -9,6 +11,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,8 +19,11 @@ import com.google.gson.Gson;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+
+import static android.app.DatePickerDialog.*;
 
 /**
  * Add Driver view
@@ -53,11 +59,15 @@ public class AddDriverView extends Activity {
      */
     Button createAccountButton;
 
+    Calendar birthday;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_driver);
 
+        birthday = Calendar.getInstance();
+        context = this;
         //defining view objects
         usernameEditText = (EditText) findViewById(R.id.username_add_account_edit_text);
         dobEditText = (EditText) findViewById(R.id.dob_add_account_edit_text);
@@ -66,87 +76,35 @@ public class AddDriverView extends Activity {
         creditEditText = (EditText) findViewById(R.id.credit_add_account_edit_text);
         createAccountButton = (Button) findViewById(R.id.create_account_button);
 
+
         //text formatting listener for phone edit text
         phoneEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+
 
         dobEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogFragment frag = new DateSelector();
-                frag.show(getFragmentManager(), "DatePicker");
-            }
+                DatePickerDialog frag = new DatePickerDialog(context, changeBirthday, birthday.get(Calendar.YEAR), birthday.get(Calendar.MONTH), birthday.get(Calendar.DAY_OF_MONTH));
+                frag.show();
+            };
         });
         //Create account Button code.
         createAccountButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //all edit text parsing logic
-
-                //if the username edit text is empty or hasn't been changed
-                String formattedNameString = usernameEditText.getText().toString().trim();
-                if(TextUtils.isEmpty(formattedNameString)){
-                    usernameEditText.setError("The Name Field cannot be empty.");
-                    return;
-                }
-
-                //if the dob edit text is empty or hasn't been changed
-                String formattedDateString = dobEditText.getText().toString().trim();
-                if(TextUtils.isEmpty(formattedDateString)){
-                    dobEditText.setError("The Date Field cannot be empty. It must be in format YYYY/MM/DD.");
-                    return;
-                }
-                Date DOB = returnsValidDate(formattedDateString);
-                if((DOB == null)){
-                    dobEditText.setError("Your date is not a valid date. It must be in format YYYY/MM/DD.");
-                    return;
-                }
-
-                //if the email edit text is empty or hasn't been changed
-                String formattedEmailString = emailEditText.getText().toString().trim();
-                if(TextUtils.isEmpty(formattedEmailString)){
-                    emailEditText.setError("The Email Field cannot be empty, " +
-                            "and you must provide an email using the pattern john@example.com.");
-                    return;
-                }
-                if(!android.util.Patterns.EMAIL_ADDRESS.matcher(formattedEmailString).matches()){
-                    //got idea from http://stackoverflow.com/questions/12947620/email-address-validation-in-android-on-edittext?noredirect=1&lq=1
-                    //from answer by user1737884
-                    emailEditText.setError("A valid email with the pattern john@example.com must be used.");
-                    return;
-                }
-
-                //if the phone edit text is empty or hasn't been changed
-                String unformattedPhoneString = phoneEditText.getText().toString().trim();
-                //code is commented out as it doesn't currently work, and doesn't need to at this moment
-                //TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-                String formattedPhoneString = PhoneNumberUtils.formatNumber(unformattedPhoneString);
-                //gets country code from sim card
-                //idea from http://stackoverflow.com/questions/12210696/how-to-get-country-or-its-iso-code from Sahil Mahajan Mj
-                if(TextUtils.isEmpty(formattedPhoneString)){
-                    phoneEditText.setError("The Phone Field cannot be empty, and must be of pattern (123) 456-7890, or 1 123-456-7890.");
-                    return;
-                }
-                if(formattedPhoneString.length() != 14){
-                    phoneEditText.setError("The Phone Field cannot be empty, and must be of pattern (123) 456-7890, or 1 123-456-7890.");
-                    return;
-                }
-
-                //if the credit edit text is empty or hasn't been changed
-                //I didn't think parsing credit info was important at this moment, but here's how to do it
-                //http://stackoverflow.com/questions/11790102/format-credit-card-in-edit-text-in-android
-                String formattedCreditString = creditEditText.getText().toString().trim();
-                if(TextUtils.isEmpty(formattedCreditString)){
-                    creditEditText.setError("The Credit Card Field cannot be empty, and pattern must be exactly XXXXBBBBYYYYAAAA.");
-                    return;
-                }
-                if(formattedCreditString.length() != 16){
-                    creditEditText.setError("The Credit Card Field must be 16 characters in length, and pattern must be exactly XXXXBBBBYYYYAAAA.");
+                String creditCard = creditEditText.getText().toString().trim();
+                String username = usernameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String birthday = dobEditText.getText().toString().trim();
+                String phoneNumber = PhoneNumberUtils.formatNumber(phoneEditText.getText().toString().trim(), "Canada");
+                if(checkForIncorrectEntries(username, birthday, email, phoneNumber, creditCard)) {
                     return;
                 }
 
                 //make the objects
-                User user = new User(formattedNameString, DOB,
-                        formattedCreditString, formattedEmailString, formattedPhoneString);
+                Driver user = new Driver(username, birthday,
+                        creditCard, email, phoneNumber);
 
                 //check that account doesn't already exist
                 AsyncController controller = new AsyncController();
@@ -178,34 +136,78 @@ public class AddDriverView extends Activity {
             }
         });
 
+    }
+
+
+    private Boolean checkForIncorrectEntries(String formattedName, String birthday, String formattedEmail, String phoneNumber, String creditCard){
+
+
+        //if the username edit text is empty or hasn't been changed
+        if(TextUtils.isEmpty(formattedName)){
+            usernameEditText.setError("The Name Field cannot be empty.");
+            return true;
+        }
+
+        //if the dob edit text is empty or hasn't been changed
+
+        if(TextUtils.isEmpty(birthday)){
+            dobEditText.setError("The Date Field cannot be empty. It must be in format YYYY/MM/DD.");
+            return true;
+        }
+
+        //if the email edit text is empty or hasn't been changed
+        if(TextUtils.isEmpty(formattedEmail)){
+            emailEditText.setError("The Email Field cannot be empty, " +
+                    "and you must provide an email using the pattern john@example.com.");
+            return true;
+        }
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(formattedEmail).matches()){
+            //got idea from http://stackoverflow.com/questions/12947620/email-address-validation-in-android-on-edittext?noredirect=1&lq=1
+            //from answer by user1737884
+            emailEditText.setError("A valid email with the pattern john@example.com must be used.");
+            return true;
+        }
+
+        if(phoneNumber.length() != 14){
+            phoneEditText.setError("The Phone Field cannot be empty, and must be of pattern (123) 456-7890, or 1 123-456-7890.");
+            return true;
+        }
+
+        //gets country code from sim card
+        //idea from http://stackoverflow.com/questions/12210696/how-to-get-country-or-its-iso-code from Sahil Mahajan Mj
+        if(TextUtils.isEmpty(phoneNumber)) {
+            phoneEditText.setError("The Phone Field cannot be empty, and must be of pattern (123) 456-7890, or 1 123-456-7890.");
+            return false;
+        }
+            //if the credit edit text is empty or hasn't been changed
+        //I didn't think parsing credit info was important at this moment, but here's how to do it
+        //http://stackoverflow.com/questions/11790102/format-credit-card-in-edit-text-in-android
+
+        if(TextUtils.isEmpty(creditCard)){
+            creditEditText.setError("The Credit Card Field cannot be empty, and pattern must be exactly XXXXBBBBYYYYAAAA.");
+            return true;
+        }
+        if(creditCard.length() != 16){
+            creditEditText.setError("The Credit Card Field must be 16 characters in length, and pattern must be exactly XXXXBBBBYYYYAAAA.");
+            return true;
+        }
 
 
 
+        return false;
     }
 
     /**
-     * Returns a date if the input string is a valid date, and null if it isn't
-     * @param formattedDateString
+     * Listener for when date is changed in a date dialog, sets the dogEditText to the date chosen
+     * by the user
      * @return
      */
-    private Date returnsValidDate(String formattedDateString){
-        //got idea from http://www.java2s.com/Tutorial/Java/0120__Development/CheckifaStringisavaliddate.htm
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH);
-        Date inputDate;
-        Date currentDate = new Date();
-
-        dateFormat.setLenient(false);
-        try {
-            inputDate = dateFormat.parse(formattedDateString);
-        } catch (ParseException pe) {
-            //date was not in correct format
-            return null;
+    private OnDateSetListener changeBirthday = new OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            birthday.set(year, month, dayOfMonth);
+            SimpleDateFormat date = new SimpleDateFormat("MMM dd yyyy");
+            dobEditText.setText(date.format(birthday.getTime()));
         }
-        if(inputDate.after(currentDate)){
-            //date of birth is after current date, shouldn't happen
-            return null;
-        }
-        return inputDate;
-    }
+    };
 }
